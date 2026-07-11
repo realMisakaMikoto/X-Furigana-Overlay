ec73d73edf1f2abbd28086c55d2338eb3fc5c086
unknown <afunnypersonlol0@gmail.com>
2026-07-11 21:29:19 +0800

    chore: bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.sosdanfurigana"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
     }
 
     buildTypes {
